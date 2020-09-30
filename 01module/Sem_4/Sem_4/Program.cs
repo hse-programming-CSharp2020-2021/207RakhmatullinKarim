64418b672bf49646cbe7140242eb7e908bae02de
Карим Рахматуллин <karimrakhmatullin@MacBook-Pro-Karim.local>
2020-09-30 18:19:47 +0300

    Honework 1_04

---
+++ 01module/Sem_4/Sem_4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sem_4
+{
+    class Program
+    {
+        public static bool Shift(ref char ch)
+        {
+            if (!(((int)ch >= (int)('a')) && ((int)ch < ((int)('z') + 1))) ||
+                !char.TryParse(Console.ReadLine(), out ch))
+            {
+                Console.WriteLine("Error");
+                return false;
+            }
+            ch = (char)(((int)ch - int('a') + 4) % 26);
+            return true;
+
+        }
+        static void Main(string[] args)
+        {
+            char ch;
+            if (!char.TryParse(Console.ReadLine(), out ch) ||
+                !(((char)ch > 'a') && ((char)ch < ('z' + 1))))
+            {
+                Console.WriteLine("Error");
+                return;
+            }
+            if (Shift(ref ch))
+            {
+                Console.WriteLine((char)ch);
+            }
+        }
+    }
+}
