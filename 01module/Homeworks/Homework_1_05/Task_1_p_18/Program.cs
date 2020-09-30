4e33bcfb3522250efff9f0ab807f020fa334d1eb
Карим Рахматуллин <karimrakhmatullin@MacBook-Pro-Karim.local>
2020-09-30 20:37:19 +0300

    Pushing Homewor_1_05

---
+++ 01module/Homeworks/Homework_1_05/Task_1_p_18/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Task_1_p_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            do
+            {
+                Console.WriteLine("Input the length of array");
+            } while ((!int.TryParse(Console.ReadLine(), out n)) ||
+            (n <= 0) || (n >= 32));
+            Printer(GenerateArray(n));
+        }
+
+        static public void Printer(long[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        public static long[] GenerateArray(int n)
+        {
+            long[] array = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = (long)Math.Pow(2, i);
+            }
+            return array;
+        }
+    }
+}
---
+++ 01module/Homeworks/Homework_1_05/Task_8_p_16/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task_8_p_16
+{
+    class Program
+    {
+        public static double[] GenerateArr(int n)
+        {
+            double[] arr = new double[n];
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = ((i * (i + 1)) / 2) % n;
+            }
+            return arr;
+        }
+
+        public static double[] ArrToNormal(double[] arr)
+        {
+            double max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (max < arr[i])
+                {
+                    max = arr[i];
+                }
+
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] /= max;
+            }
+            return arr;
+        }
+
+        static public void Printer(double[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]:f3} ");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Input the length of array.");
+            while ((!int.TryParse(Console.ReadLine(), out n)) || (n < 1))
+            {
+                Console.WriteLine("Try again");
+            }
+            double[] array = GenerateArr(n);
+            Printer(array);
+            double[] normalArray = ArrToNormal(array);
+            Printer(normalArray);
+
+
+
+        }
+    }
+}
+
