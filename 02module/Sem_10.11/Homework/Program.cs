c7c0c5782a0744d946018ec99aeb1a124946e7a5
Карим Рахматуллин <mirak4319@gmail.com>
2020-11-11 00:58:23 +0300

    3.10

---
+++ 02module/Sem_10.11/Homework/Program.cs
@@ -0,0 +1,137 @@
+﻿using System;
+
+namespace Task_8
+{
+    class Point
+    {
+        double x;
+        double y;
+        public Point() { }
+        public double dest(double x1, double y1)
+        {
+            double deltaX = x - x1;
+            double deltaY = y - y1;
+            return Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+        }
+        public Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public double X
+        {
+            get
+            {
+                return x;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return y;
+            }
+        }
+
+    }
+    class Triangle
+    {
+        Point A;
+        Point B;
+        Point C;
+        public Triangle() { }
+        public Triangle(Point A, Point B, Point C)
+        {
+            this.A = A;
+            this.B = B;
+            this.C = C;
+        }
+        public Triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+        {
+            this.A = new Point(x1, y1);
+            this.B = new Point(x2, y2);
+            this.C = new Point(x3, y3);
+        }
+        public double AB
+        {
+            get
+            {
+                return A.dest(B.X, B.Y);
+            }
+        }
+        public double BC
+        {
+            get
+            {
+                return B.dest(C.X, C.Y);
+            }
+        }
+        public double AC
+        {
+            get
+            {
+                return A.dest(C.X, C.Y);
+            }
+        }
+        public double Perimetr
+        {
+            get { return AB + AC + BC; }
+        }
+        public double Square
+        {
+            get
+            {
+                double halfP = (AB + BC + AC) / 2;
+                return Math.Sqrt(halfP * (halfP - AB) * (halfP - BC) * (halfP - AC));
+            }
+        }
+        public override string ToString()
+        {
+            string res = $"AB = {AB:f2} BC = {BC:f2} AC = {AC:f2} Perimetr = {Perimetr:f2} Square = {Square:f2}";
+            return res;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Random rnd = new Random();
+            n = rnd.Next(5, 16);
+            Triangle[] arrOfTriangle = new Triangle[n];
+            for (int i = 0; i < n; i++)
+            {
+                Point A = new Point(-10 + rnd.NextDouble() * (10 - (-10)), -10 + rnd.NextDouble() * (10 - (-10)));
+                Point B = new Point(-10 + rnd.NextDouble() * (10 - (-10)), -10 + rnd.NextDouble() * (10 - (-10)));
+                Point C = new Point(-10 + rnd.NextDouble() * (10 - (-10)), -10 + rnd.NextDouble() * (10 - (-10)));
+                arrOfTriangle[i] = new Triangle(A, B, C);
+            }
+            for (int i = 0; i < arrOfTriangle.Length; i++)
+            {
+                Console.WriteLine(arrOfTriangle[i].ToString());
+            }
+            Console.WriteLine();
+            SortSquares(arrOfTriangle);
+            for (int i = 0; i < arrOfTriangle.Length; i++)
+            {
+                Console.WriteLine(arrOfTriangle[i].ToString());
+            }
+        }
+
+        private static void SortSquares(Triangle[] arrOfTriangle)
+        {
+            for (int i = arrOfTriangle.Length - 1; i > 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (arrOfTriangle[i].Square > arrOfTriangle[j].Square)
+                    {
+                        Triangle tmp = arrOfTriangle[i];
+                        arrOfTriangle[i] = arrOfTriangle[j];
+                        arrOfTriangle[j] = tmp;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ 02module/Sem_3.11/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Task_7
+{
+    class Program
+    {
+        class Sin
+        {
+            double Xmin, Xmax;
+            public Sin(double min, double max)
+            {
+                Xmin = min;
+                Xmax = max;
+            }
+            public double this[double index]
+            {
+                get
+                {
+                    if((index < 0) || (index > Xmax))
+                    {
+                        throw new ArgumentException("Index out of range.");
+                    }
+                    return Math.Sin(Xmin + Math.PI / 6 * index);
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Sin f = new Sin(0, Math.PI);
+            for(double i = 0; i < Math.PI; i += Math.PI / 6)
+            {
+                Console.WriteLine(f[i]);
+            }
+        }
+    }
+}
